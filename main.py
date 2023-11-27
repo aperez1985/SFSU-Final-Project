26396a4cb5b9528b4e847f89fa7441c7f0e3cfa4
aperez1985 <149124939+aperez1985@users.noreply.github.com>
2023-11-27 08:48:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import time
+
 # Initialize Pygame
 pygame.init()
 
@@ -48,6 +49,7 @@ coordinates = []
 score = 0
 life = 3
 
+
 class Missile(pygame.sprite.Sprite):
     def __init__(self, start_x, start_y, target_x, target_y):
         super().__init__()
@@ -56,58 +58,43 @@ class Missile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (start_x, start_y)
         self.target_x = target_x
-        self.target_y = target_y +30
+        self.target_y = target_y + 30
         self.speed = 2
         self.finished = False  # Flag to check if the missile is finished
 
     def update(self):
         if not self.finished:
-            direction_x = self.target_x  - self.rect.centerx
+            direction_x = self.target_x - self.rect.centerx
             direction_y = self.target_y - self.rect.centery
             distance = ((direction_x) ** 2 + (direction_y) ** 2) ** 0.5
-            #print(target_x , target_y)
             new_missile_x = (target_x - origin_x) // scale
             new_missile_y = (origin_y - target_y) // scale
-            print(new_missile_x , new_missile_y)
-            #print(self.rect.x , self.rect.y, target_x, target_y)
 
             if distance != 0:
                 self.rect.x += (direction_x / distance) * self.speed
                 self.rect.y += (direction_y / distance) * self.speed
-                # Print the collision coordinates
+
                 asteroid_x = (asteroid.rect.x - origin_x) // scale
                 asteroid_y = (origin_y - asteroid.rect.y) // scale
                 asteroid_x = asteroid_x + 1
-                print(f"Collision Coordinates: ({asteroid_x}, {asteroid_y})")
-                
 
-                # Check for collision with the asteroid
                 if self.rect.colliderect(asteroid.rect) and asteroid_y == new_missile_y and asteroid_x == new_missile_x:
-                    
-
-                    # Your code for handling the collision, such as creating an explosion
-                    explosion = Explosion(self.target_x, self.target_y-20)
+                    explosion = Explosion(self.target_x, self.target_y - 20)
                     explosion_group.add(explosion)
 
-                    # Remove the missile and asteroid from their respective groups
                     missile_group.remove(self)
                     asteroid.reset_position()
                     global score
                     score = score + 10
                     print(f"score = {score}")
         if self.rect.x == target_x - 7 and self.rect.y == target_y:
-              #self.finished = True
-              # Your code for handling the collision, such as creating an explosion
-              explosion = Explosion(self.target_x, self.target_y-20)
-              explosion_group.add(explosion)
+            explosion = Explosion(self.target_x, self.target_y - 20)
+            explosion_group.add(explosion)
 
-                # Remove the missile and asteroid from their respective groups
-              missile_group.remove(self)
-              global life
-              life = life - 1
-              print(f"life = {life}")
-
-
+            missile_group.remove(self)
+            global life
+            life = life - 1
+            print(f"life = {life}")
 
 
 class Explosion(pygame.sprite.Sprite):
@@ -144,15 +131,13 @@ class Asteroid(pygame.sprite.Sprite):
         self.rect.x = random.randint(160, screen_width - 160)
         self.rect.y = random.randint(100, screen_height - 100)
 
-        # Change the PyCharm coordinate to Cartesian coordinate
         x_cartesian = (self.rect.x - origin_x) // scale
         y_cartesian = (origin_y - self.rect.y) // scale
 
-        # Ensure the adjusted coordinates are within the coordinate plane boundaries
         x_cartesian = max(-int(coord_plane_width / (2 * scale)), min(int(coord_plane_width / (2 * scale)), x_cartesian))
-        y_cartesian = max(-int(coord_plane_height / (2 * scale)), min(int(coord_plane_height / (2 * scale)), y_cartesian))
+        y_cartesian = max(-int(coord_plane_height / (2 * scale)),
+                          min(int(coord_plane_height / (2 * scale)), y_cartesian))
 
-        # Set the asteroid's rect attributes to match the adjusted coordinates
         self.rect.x = origin_x - 15 + x_cartesian * scale
         self.rect.y = origin_y - 12 - y_cartesian * scale
 
@@ -161,6 +146,7 @@ class Asteroid(pygame.sprite.Sprite):
     def update(self):
         pass  # Add any asteroid-specific update logic if needed
 
+
 class EarthDestroyed(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -168,20 +154,50 @@ class EarthDestroyed(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (screen_width // 2, screen_height // 2)
 
+
 earth_destroyed_group = pygame.sprite.Group()
 missile_group = pygame.sprite.Group()
 explosion_group = pygame.sprite.Group()
 asteroid_group = pygame.sprite.Group()
+
+
+
 
 def reset_game_state():
     global missile, asteroid
     missile = None
     asteroid.reset_position()
 
+def reset_game():
+    global life, score
+    life = 3
+    score = 0
+    reset_game_state()
+
+
+def show_game_over():
+    game_over_font = pygame.font.SysFont('times new roman', 50)
+
+    # Render "Game Over" text
+    game_over_text = game_over_font.render('Game Over...', True, (0, 0, 255))
+    game_over_text_rect = game_over_text.get_rect(
+        center=(screen_width // 2, screen_height // 2 - game_over_text.get_height()))
+
+    # Render "You failed to save the Earth" text
+    fail_text = game_over_font.render('You Failed to Save the Earth!!', True, (0, 0, 255))
+    fail_text_rect = fail_text.get_rect(center=(screen_width // 2, screen_height // 2 + fail_text.get_height()))
+
+    # Display both lines
+    screen.blit(game_over_text, game_over_text_rect)
+    screen.blit(fail_text, fail_text_rect)
+
+    pygame.display.flip()
+
 def show_earth_destroyed():
     earth_destroyed_group.update()
     earth_destroyed_group.draw(screen)
     pygame.display.flip()
+
 
 def wait_for_restart():
     waiting_for_restart = True
@@ -193,101 +209,64 @@ def wait_for_restart():
                     reset_game_state()
                     earth_destroyed_group.remove(earth_destroyed)
                     break
+
+
 def display_score():
     score_font = pygame.font.SysFont('times new roman', 20)
     score_surface = score_font.render('Score: ' + str(score), True, box_color)
     life_surface = score_font.render('Life: ' + str(life), True, box_color)
     screen.blit(score_surface, (200, 0))
     screen.blit(life_surface, (300, 0))
-    
 
 
-# ... (your existing imports and code)
-
-asteroid = Asteroid()  # Initialize the asteroid
+asteroid = Asteroid()
 asteroid_group.add(asteroid)
 
 earth_destroyed = EarthDestroyed()
 earth_destroyed_group.add(earth_destroyed)
 
-running = True  # Initialize the running variable
+running = True
 
-missile = None  # Initialize missile here
+missile = None
 
-
-while running:
-    # Step 1: an asteroid1.png randomly appears on the cartesian coordinate.
-    asteroid.reset_position()
-
-    while running:
+while running and life > 0:
+        # Event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
+                    # Handle user input and create a new missile
                     if missile and missile.finished:
                         if user_text.strip() == f"{(asteroid.rect.x - origin_x) // scale}, {(origin_y - asteroid.rect.y) // scale}":
-                            # Step 5: if the user entered coordinate match with the location of the asteroid1.png
-                            #   this asteroid1.png disappear and another asteroid1.png randomly appears on the coordinate plane.
                             asteroid.reset_position()
                         else:
                             print("Missile coordinates do not match the asteroid. Try again.")
-                            # Step 5: if the user entered a coordinate that does not match the location of the asteroid1.png
-                            #   the asteroid1.png will blit to the left until it gets to x=10,
-                            #   then an “EarthDestroyed.png” will appear on the screen.
-                            while asteroid.rect.x > 10:
-                                asteroid.rect.x -= 3
-                                screen.fill((0, 0, 0))
-                                screen.blit(background_image, (0, 0))
+                            while asteroid.rect.x > 0:
 
-                                # Draw the x and y axes
-                                pygame.draw.line(screen, (255, 255, 255), (160, origin_y), (screen_width - 160, origin_y), 2)
-                                pygame.draw.line(screen, (255, 255, 255), (origin_x, 100), (origin_x, screen_height - 100), 2)
+                                # Show EarthDestroyed.png
+                                show_earth_destroyed()
+                                pygame.time.delay(2000)  # Wait for 2 seconds before restarting
 
-                                # Draw gridlines
-                                for x in range(160, screen_width - 160, scale):
-                                    pygame.draw.line(screen, gridline_color, (x, 100), (x, screen_height - 100), 1)
-                                for y in range(100, screen_height - 100, scale):
-                                    pygame.draw.line(screen, gridline_color, (160, y), (screen_width - 160, y), 1)
+                                # Show "Game Over"
+                                show_game_over()
+                                pygame.time.delay(2000)  # Wait for 2 seconds before quitting
+                                quit()
 
-                                # Render and draw the axis labels
-                                x_label_surface = font.render(x_label, True, (255, 255, 255))
-                                screen.blit(x_label_surface, (screen_width - x_label_surface.get_width() - 80, origin_y + 10))
+                                # Break out of the inner loop
+                                break
 
-                                y_label_surface = font.render(y_label, True, (255, 255, 255))
-                                screen.blit(y_label_surface, (origin_x + 10, 70))
+                                # Restart the game
+                            reset_game()
 
-                                asteroid_group.update()
-                                asteroid_group.draw(screen)
-
-                                # Reset missile and create a new one
-                                missile = Missile(origin_x, screen_height, target_x, target_y)
-                                missile_group.add(missile)
-
-                                missile_group.update()
-                                missile_group.draw(screen)
-
-                                explosion_group.update()
-                                explosion_group.draw(screen)
-
-                                # Render and display the user's text input
-                                text_surface = font.render(f"({user_text})", True, text_color)
-                                box_width = text_surface.get_width() + 10
-                                box_height = text_surface.get_height() + 10
-                                pygame.draw.rect(screen, box_color, (text_x - 5, text_y - 5, box_width, box_height))
-                                screen.blit(text_surface, (text_x, text_y))
-
-                                pygame.display.flip()
-
-                            show_earth_destroyed()
-                            wait_for_restart()
+                            # Break out of the inner loop
+                            break
 
                     try:
                         user_text = user_text.replace(" ", "")
                         x, y = map(int, user_text.split(","))
                         target_x = origin_x + x * scale
                         target_y = origin_y - y * scale
-                        # Reset missile and create a new one
                         missile = Missile(origin_x, screen_height, target_x, target_y)
                         missile_group.add(missile)
                     except (ValueError, IndexError):
@@ -298,20 +277,18 @@ while running:
                 else:
                     user_text += event.unicode
 
+
         screen.fill((0, 0, 0))
         screen.blit(background_image, (0, 0))
 
-        # Draw the x and y axes
         pygame.draw.line(screen, (255, 255, 255), (160, origin_y), (screen_width - 160, origin_y), 2)
         pygame.draw.line(screen, (255, 255, 255), (origin_x, 100), (origin_x, screen_height - 100), 2)
 
-        # Draw gridlines
         for x in range(160, screen_width - 160, scale):
             pygame.draw.line(screen, gridline_color, (x, 100), (x, screen_height - 100), 1)
         for y in range(100, screen_height - 100, scale):
             pygame.draw.line(screen, gridline_color, (160, y), (screen_width - 160, y), 1)
 
-        # Render and draw the axis labels
         x_label_surface = font.render(x_label, True, (255, 255, 255))
         screen.blit(x_label_surface, (screen_width - x_label_surface.get_width() - 80, origin_y + 10))
 
@@ -328,7 +305,6 @@ while running:
         explosion_group.update()
         explosion_group.draw(screen)
 
-        # Render and display the user's text input
         text_surface = font.render(f"({user_text})", True, text_color)
         box_width = text_surface.get_width() + 10
         box_height = text_surface.get_height() + 10
@@ -336,4 +312,12 @@ while running:
         screen.blit(text_surface, (text_x, text_y))
 
         pygame.display.flip()
+
+    # Display EarthDestroyed.png when life is 0
+        if life == 0:
+            show_earth_destroyed()
+            show_game_over()
+            pygame.time.delay(10000)  # Wait for 5 seconds before restarting
+            quit()
+
 pygame.quit()
